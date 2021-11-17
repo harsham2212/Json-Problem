19f7dd76305998bb59ea34b2077be4e38f78ec35
Harsh Singh <kmrharsh50@gmail.com>
2021-11-17 10:22:12 +0530

    [Harsh]Added:features InventoryJson Features Implemented

---
+++ ObjecteOrientedProgram/InventoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgram
+{
+    class InventoryModel
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ ObjecteOrientedProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            InventoryMain im = new InventoryMain();
+            while (flag)
+            {
+                Console.WriteLine("Enter option \n 1.Name \n 2.Weight \n 3.Price \n 4.Exit \n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        im.DisplayData(@"E:\BridgeLAbz\Git\ObjectOrientedPrograms\ObjecteOrientedProgram\File\Inventory.json");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
